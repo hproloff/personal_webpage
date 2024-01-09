1076e696dae11c271088ae784b8c698943cda19c
Harrison <hproloff@gmail.com>
2024-01-09 12:23:48 -0600

    touchup for style, file cleaning

--- src/components/SocialButtons.tsx
+++ src/components/SocialButtons.tsx
@@ -5,10 +5,10 @@ const SocialButtons: React.FC = () => {
     <div className="flex justify-center md:justify-start pt-5 space-x-5">
       {/* Instagram */}
       <a
-        href="#" // Insert Instagram link
+        href="https://www.instagram.com/harrison.roloff/" // Insert Instagram link
         target="_blank"
         rel="noopener noreferrer"
-        className="socialLink hover:animate-pulse hover:shadow-md ease-in-out"
+        className="socialLink hover:animate-pulse hover:shadow-md ease-in-out dark:bg-gray-700 bg-white"
       >
         <img
           src="/instagram_icon.png" // Path to Instagram icon
@@ -19,10 +19,10 @@ const SocialButtons: React.FC = () => {
 
       {/* LinkedIn */}
       <a
-        href="#" // Insert LinkedIn link
+        href="https://www.linkedin.com/in/harrison-roloff/" // Insert LinkedIn link
         target="_blank"
         rel="noopener noreferrer"
-        className="socialLink hover:animate-pulse hover:shadow-md ease-in-out"
+        className="socialLink hover:animate-pulse hover:shadow-md ease-in-out dark:bg-gray-700 bg-white"
       >
         <img
           src="linkedin_icon.png" // Path to LinkedIn icon
@@ -36,7 +36,7 @@ const SocialButtons: React.FC = () => {
         href="#" // Insert Facebook link
         target="_blank"
         rel="noopener noreferrer"
-        className="socialLink hover:animate-pulse hover:shadow-md ease-in-out"
+        className="socialLink hover:animate-pulse hover:shadow-md ease-in-out dark:bg-gray-700 bg-white"
       >
         <img
           src="/facebook_icon.png" // Path to Facebook icon
@@ -47,10 +47,10 @@ const SocialButtons: React.FC = () => {
 
       {/* GitHub */}
       <a
-        href="#" // Insert GitHub link
+        href="https://github.com/hproloff" // Insert GitHub link
         target="_blank"
         rel="noopener noreferrer"
-        className="socialLink hover:animate-pulse hover:shadow-md ease-in-out"
+        className="socialLink hover:animate-pulse hover:shadow-md ease-in-out dark:bg-gray-700 bg-white"
       >
         <img
           src="/github_icon.png" // Path to GitHub icon
