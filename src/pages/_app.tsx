0670e8a253202acc5d2c43d4e04cbe435087355c
Harrison <hproloff@gmail.com>
2024-01-10 11:37:38 -0600

    update next-themes to resolve error

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,12 @@
 import "tailwindcss/tailwind.css";
 import "../styles/globals.css";
-import { ThemeProvider } from "next-themes";
+import { ThemeProvider } from 'next-themes';
 import { AppProps } from 'next/app';
 
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
   return (
-    <ThemeProvider attribute="class">
-      <div>
+    <ThemeProvider attribute="class" defaultTheme="dark">
         <Component {...pageProps} />
-      </div>
     </ThemeProvider>
   );
 };
